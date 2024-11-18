454c64eedeb46d1d2ad4f7f106e7956538e56957
Ethan Zhang <ethanz8382@gmail.com>
2024-11-17 21:08:18 -0800

    added functionality to support extending training

--- GNNRNN_train.py
+++ GNNRNN_train.py
@@ -14,10 +14,27 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import RandomSampler
 from datetime import datetime
 import gc
+import argparse
+import sys
 
 from utils.data_utils import MasterDataModule
 from config import *
 from models.GNNRNN import GNNRNN
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Script to train GNNRNN model")
+    parser.add_argument('--extend', action='store_true', help="Extending a model's training")
+    parser.add_argument('-f', type=str, help="Path to checkpoint file", required=False)
+    parser.add_argument('-e', type=int, help="New number of epochs to train model for", required=False)
+    args = parser.parse_args()
+
+    if args.extend:
+        if args.f is None:
+            raise ValueError("Error: checkpoint required for extending training")
+    if args.e and not args.extend:
+        raise ValueError("Error: epochs provided for non-extension training run")
+    
+    return args
 
 def setup_training_dirs(experiment_name: str = None, root_dir: str = "./", timestamp: str = None):
     # Create directories if they don't exist
@@ -42,16 +59,17 @@ def main():
     gc.collect()
     torch.cuda.empty_cache()
 
-    experiment_name = EXPERIMENT_NAME
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    args = parse_args()
+    extend = args.extend
+    epochs = EPOCHS if not extend else args.e
+    checkpoint_path = None if not extend else args.f
+    checkpoint_dir = None if not checkpoint_path else os.path.dirname(checkpoint_path) 
+    experiment_name = EXPERIMENT_NAME if not extend else f'Extending_to_{epochs}_epochs'
 
-    # Setting up training directories
-    checkpoint_dir, logs_dir = setup_training_dirs(experiment_name, timestamp=timestamp)
-
-    logger = WandbLogger(
-        save_dir=str(logs_dir),
-        name=experiment_name
-    )
+    if not extend:
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        # Setting up training directories
+        checkpoint_dir, logs_dir = setup_training_dirs(experiment_name, timestamp=timestamp)
 
     checkpoint_callback = ModelCheckpoint(
         dirpath=str(checkpoint_dir),
@@ -61,18 +79,24 @@ def main():
         mode='min',
         save_last=True, # save last model
     )
+    
+    logger = WandbLogger(
+            # save_dir=str(logs_dir),
+            save_dir='./logs', # bad
+            name=experiment_name
+        )
 
     model = GNNRNN(graph_emb_dim=GCN_EMB_DIM, 
-        enc_hidden_dim=ENC_HIDDEN_DIM, 
-        output_length=NUM_OUTPUT_MONTHS,
-        gcn_layers=GCN_NUM_LAYERS,
-        gcn_dropout=GCN_DROPOUT,
-        lr=LEARNING_RATE)
+            enc_hidden_dim=ENC_HIDDEN_DIM, 
+            output_length=NUM_OUTPUT_MONTHS,
+            gcn_layers=GCN_NUM_LAYERS,
+            gcn_dropout=GCN_DROPOUT,
+            lr=LEARNING_RATE)
 
     data_module = MasterDataModule(batch_size=NUM_INPUT_MONTHS *  BATCH_SIZE)
 
     trainer = pl.Trainer(
-        max_epochs=EPOCHS,
+        max_epochs=epochs,
         callbacks=[checkpoint_callback],
         logger=logger,
         default_root_dir=str(checkpoint_dir),
@@ -81,7 +105,8 @@ def main():
         log_every_n_steps=1
     )
 
-    trainer.fit(model, datamodule=data_module)
+    trainer.fit(model, datamodule=data_module, ckpt_path=checkpoint_path)
+
 
 if __name__ == "__main__":
     main()
