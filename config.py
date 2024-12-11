f4cabebb9c62fa53ff6525ae0cfc138bd0987215
Ethan Zhang <ethanz8382@gmail.com>
2024-12-11 15:22:57 -0800

    new config parameters

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 # Pretraining parameters
-EPOCHS = 10
+EPOCHS = 4
 LEARNING_RATE = 1e-4
-EXPERIMENT_NAME = 'GATLSTM_newdata_large'
-BATCH_SIZE = 16 # number of batches of 36
+EXPERIMENT_NAME = 'GCNRNN_small'
+BATCH_SIZE = 32 # number of batches of 36
 
 # Finetuning parameters
 FT_EPOCHS = 5
 FT_LEARNING_RATE = 1e-5
-FT_BATCH_SIZE = 16
-FT_EXPERIMENT_NAME = '5epochs_1e-5'
+FT_BATCH_SIZE = 4
+FT_EXPERIMENT_NAME = '5epochs_ege_frozen'
 
 # Model parameters
-GENET_EMB_DIM = 128
-GENET_NUM_LAYERS = 7
+GENET_EMB_DIM = 64
+GENET_NUM_LAYERS = 5
 GENET_DROPOUT = 0.3
-ENC_HIDDEN_DIM = 128
+ENC_HIDDEN_DIM = 64
 
 # Model architecture
-NODE_EMBEDDER = 'GAT' # GAT or GCN
-ENC_DEC = 'LSTM' # RNN or LSTM
-ADJACENCY = 'grid' # Options: grid, any new options added afterwards...
+NODE_EMBEDDER = 'GCN' # GAT or GCN
+ENC_DEC = 'RNN' # RNN or LSTM
+ADJACENCY = 'multimesh1' # Options: grid, any new options added afterwards...
